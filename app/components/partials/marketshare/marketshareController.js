e9144d3466965216b84418f72c039adfe31aeb04
ritesh91188 <91188ritesh@gmail.com>
2018-03-18 21:25:29 +0530

    added piechart for  market share

--- app/components/partials/marketshare/marketshareController.js
+++ app/components/partials/marketshare/marketshareController.js
@@ -7,6 +7,27 @@ var map;
     function marketshareController($scope, dashboardService) {
         var vm = this;
 
+        vm.data = [[['a', 25], ['b', 14], ['c', 7]]];
+
+        vm.chartOptions = {
+            title:'Market Share',
+            //  gridPadding: {top:0, bottom:38, left:0, right:0},
+            seriesDefaults: {
+                renderer: $.jqplot.PieRenderer,
+                trendline: { show: false },
+                rendererOptions: { padding: 8, showDataLabels: true }
+            },
+            legend: {
+                show: true,
+                placement: 'outside',
+                rendererOptions: {
+                    numberRows: 1
+                },
+                location: 's',
+                marginTop: '15px'
+            }
+        };
+
         $scope.$on('getcountry', function (event, args) {
             alert('Received COuntry' + args.country);
         });
